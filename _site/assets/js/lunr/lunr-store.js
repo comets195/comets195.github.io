78a7afccc165120be63504bc1058aedfa1794353
Leedongjin-nbt <lee.dongjin@nbt.com>
2020-04-20 17:01:28 +0900

    동시성 글 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "시작하는 글",
-        "excerpt":"아무것도 하지 않으면 아무일도 일어나지 않는다.   ","categories": ["시작하는 마음으로"],
+        "title": "[Raywenderlich] Concurrency 1: 소개",
+        "excerpt":"Concurrency란 무엇인가? 회사의 프로젝트를 진행하면서 iOS의 동시성 프로그래밍의 개념 공부가 필요해졌었다. 필요로 했던 기능은 이미지 업로드 서버 요청과 동시에 UI에 업로드 중이라는 변화를 보여줘야하는 기능이었다. 가장 기본적인 내용인 UI는 꼭 Main Threed에서 실행해야한다. 정도만 알고 있었고, 어떻게 다른 Threed에게 일을 시키면 되는지 알고 싶어 공부하게 되었다. 책에서는 Concurrency, 동시성을 간단하게...","categories": ["Concurrency"],
         "tags": [],
-        "url": "http://localhost:4000/%EC%8B%9C%EC%9E%91%ED%95%98%EB%8A%94%20%EB%A7%88%EC%9D%8C%EC%9C%BC%EB%A1%9C/test/",
+        "url": "http://localhost:4000/concurrency/Concurrency1/",
+        "teaser": "http://localhost:4000/assets/images/logo.jpg"
+      },{
+        "title": "Hello Swift World!",
+        "excerpt":"Hello Swift World! 안녕하세요? 첫번째 포스팅입니다. 앞으로 블로그를 어떻게 운영하면 좋을지에 대한 간략한 소개들 정도 되겠네요. 저는 iOS개발자 입니다. 아직 한참 배울께 많죠. 그래서 아는 것도 다시한번 정리해보는 시간을 갖는 것과 잘못 알고 있는 것을 피드백을 통해 고치려고합니다. 알게된 것들은 구글 검색을 통해 알게되는 것도 있겠지만 공식문서를 기반으로 알아가보려합니다. 매주...","categories": ["시작"],
+        "tags": [],
+        "url": "http://localhost:4000/%EC%8B%9C%EC%9E%91/Hello-Swift/",
         "teaser": "http://localhost:4000/assets/images/logo.jpg"
       }]
